2211a3a64aace9d1e706eb8f965c2a8d1cd50d10
Matthew Oliver <protogonoi@gmail.com>
2014-09-27 16:07:42 +1000

    Update VS projects for recent merge.

--- SMC/cdio-paranoia_version.h
+++ SMC/cdio-paranoia_version.h
@@ -1,8 +1,8 @@
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libcdio-paranoia 10.2+0.90+1"
+#define PACKAGE_STRING "libcdio-paranoia 10.2+0.93+1"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "10.2+0.90+1"
+#define PACKAGE_VERSION "10.2+0.93+1"
 
 /* Version number of package */
-#define VERSION "10.2+0.90+1"
+#define VERSION "10.2+0.93+1"
