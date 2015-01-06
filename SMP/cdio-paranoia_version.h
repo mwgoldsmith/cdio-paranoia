bc1e17e3d75426165a73dc28059f341087270ea7
Matt Oliver <protogonoi@gmail.com>
2015-01-06 17:17:28 +1100

    project: Cleanup config header.

--- SMP/cdio-paranoia_version.h
+++
@@ -1,8 +0,0 @@
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libcdio-paranoia 10.2+0.93+2"
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "10.2+0.93+2"
-
-/* Version number of package */
-#define VERSION "10.2+0.93+2"
